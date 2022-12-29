593ebc057ceec05374e416a9819c75ac1030c3f9
srimalonline <dilankasrimal23@gmail.com>
2022-12-29 11:31:15 +0530

    installing tailwind css successfully

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text className='text-red-500'>Open up App.js to start working on your app!</Text>
+    <TailwindProvider>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </TailwindProvider>
   );
 }
