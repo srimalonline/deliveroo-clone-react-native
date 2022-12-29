f9c78bac92670fe9a2653eedaa7397317af942d0
srimalonline <dilankasrimal23@gmail.com>
2022-12-29 12:11:35 +0530

    successfully tested the react navigation with tailwaind css

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
