f3d1aea6abc6b77a2402dc3c443651eb73a5e30e
srimalonline <dilankasrimal23@gmail.com>
2022-12-31 03:12:44 +0530

    Home screen icon adding

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,15 @@
 import { View, Text, SafeAreaView, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { userIcon,
-          ChevronDownIcon,
-          SearchIcon,
-          AdjustmentsIcon } from "react-native-heroicons/outline";
+import {
+  userIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsHorizontalIcon,
+  UserIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+import { TextInput } from "react-native-web";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -15,10 +20,9 @@ const HomeScreen = () => {
     });
   }, []);
   return (
-    <SafeAreaView>
-      <Text className="text-red-500">
+    <SafeAreaView className="bg-white pt-5">
+      <Text className="">
         {/* Header */}
-
         <View className="flex-row pb-3 items-center mx-4 space-x-2">
           <Image
             source={{
@@ -26,11 +30,30 @@ const HomeScreen = () => {
             }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
-          <View>
-            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-            <Text className="font-bold text-xl">Current Location</Text>
-            <ChevronDownIcon size={20} color="#00CCBB"/>
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
           </View>
+
+          <UserIcon size={35} color="#00CCBB" />
+        </View>
+
+        {/* Search */}
+        <View>
+          <View className="flex-row space-x-2 flex-1 bg-gray-200">
+            <MagnifyingGlassIcon />
+            <TextInput
+              placeholder="Restaurants and cuisines"
+              keyboardType="default"
+            />
+          </View>
+
+          <AdjustmentsHorizontalIcon color="#00CCBB"/>
         </View>
       </Text>
     </SafeAreaView>
