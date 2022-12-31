942e00e598134870d375808ec231f2ee26b2f80b
srimalonline <dilankasrimal23@gmail.com>
2022-12-31 11:12:06 +0530

    ending navigation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -44,8 +44,8 @@ const HomeScreen = () => {
         </View>
 
         {/* Search */}
-        <View className="flex-row items-center space-x-2 pb-2 mx-4">
-          <View className="flex-row space-x-2 flex-1 p-3 bg-gray-200">
+        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+          <View className="flex-row space-x-2 flex-1 p-3 bg-gray-200 px-4">
             <MagnifyingGlassIcon color="gray" size={20} />
             <TextInput
               placeholder="Restaurants and cuisines"
