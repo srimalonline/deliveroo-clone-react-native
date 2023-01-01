42ee7a5a399e210848f5c45f695460943c26d3d2
srimalonline <dilankasrimal23@gmail.com>
2023-01-02 00:01:51 +0530

    created sanity template for restaurant

---
+++ sanaty/schemas/restaurant.js
@@ -0,0 +1,44 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'restaurant',
+  title: 'Restaurant',
+  type: 'document',
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Restaurant name",
+      validation: (Rule) => Rule.required()
+    },
+    {
+      name: "short_description",
+      type: "string",
+      title: "Short Description",
+      validation: (Rule) => Rule.max(200)
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Image of the Restaurant",
+    },
+    {
+      name: "lat",
+      type: "number",
+      title: "Latitude of the Restaurant",
+    },
+    {
+      name: "long",
+      type: "number",
+      title: "Longitude of the Restaurant",
+    },
+    {
+      name: "address",
+      type: "string",
+      title: "Restaurant address",
+      validation: (Rule) =>Rule.required()
+    },
+
+  ],
+
+})
