1e7f6bf36283ea1678de3a4690e8710ae847356c
srimalonline <dilankasrimal23@gmail.com>
2023-01-02 00:43:31 +0530

    imorting featured field

--- sanaty/schemas/featured.js
+++ sanaty/schemas/featured.js
@@ -8,7 +8,7 @@ export default defineType({
     {
       name: "name",
       type: "string",
-      title: "Restaurant name",
+      title: "Featured Category name",
       validation: (Rule) => Rule.required()
     },
     {
@@ -18,48 +18,10 @@ export default defineType({
       validation: (Rule) => Rule.max(200)
     },
     {
-      name: "image",
-      type: "image",
-      title: "Image of the Restaurant",
-    },
-    {
-      name: "lat",
-      type: "number",
-      title: "Latitude of the Restaurant",
-    },
-    {
-      name: "long",
-      type: "number",
-      title: "Longitude of the Restaurant",
-    },
-    {
-      name: "address",
-      type: "string",
-      title: "Restaurant address",
-      validation: (Rule) =>Rule.required()
-    },
-    {
-      name: "rating",
-      type: "number",
-      title: "Enter a Rating from (1-5 Stars)",
-      validation: (Rule) =>
-        Rule.required()
-          .min(1)
-          .max(5)
-          .error("Please enter a Value between 1 and 5")
-    },
-    {
-      name: "type",
-      title: "Category",
-      validation: (Rule) => Rule.required(),
-      type: "reference",
-      to: [{ type: "category"}],
-    },
-    {
-      name: "deshes",
+      name: "restaurants",
       type: "array",
-      title: "Dishes",
-      of: [{ type: "reference", to: [{ type: "dish"}] }],
+      title: "Restaurants",
+      of: [{ type: "reference", to: [{ type: "restaurant"}] }],
     }
   ],
 })
--- sanaty/schemas/index.js
+++ sanaty/schemas/index.js
@@ -1,5 +1,6 @@
 import restaurant from './restaurant'
 import category from './category'
 import dish from './dish'
+import featured from './featured'
 
-export const schemaTypes = [restaurant, dish, category]
+export const schemaTypes = [restaurant, category, dish, featured]
