a331d09365bc24e5d79b3124f081d3db4d776646
srimalonline <dilankasrimal23@gmail.com>
2023-01-01 22:57:44 +0530

    restaurandcard creating

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,6 +1,7 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { ArrowRightIcon } from "react-native-heroicons/outline";
+import RestaurantCard from "./RestaurantCard";
 
 const FeaturedRow = ({id, title, description, featuredCategory}) => {
   return (
@@ -9,8 +10,22 @@ const FeaturedRow = ({id, title, description, featuredCategory}) => {
         <Text className="font-bold text-lg">{title}</Text>
         <ArrowRightIcon color="#00CCBB"/>
       </View>
-
       <Text className="text-xs text-gray-500 px-4">{description}</Text>
+
+      <ScrollView
+        horizontal
+        contentContainerStyle={{ 
+            paddingHorizontal: 15,
+        }}
+            showsHorizontalScrollIndicator={false}
+            className="pt-4"
+        >
+            {/* RestaurantCards */}
+            <RestaurantCard 
+                
+                />
+
+      </ScrollView>
     </View>
   );
 };
---
+++ components/RestaurantCard.js
@@ -0,0 +1,22 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RestaurantCard = ({
+    id,
+    imgUrl,
+    rating,
+    genre,
+    address,
+    short_description,
+    dishes,
+    long,
+    lat,
+}) => {
+  return (
+    <View>
+      <Text>RestaurantCard</Text>
+    </View>
+  )
+}
+
+export default RestaurantCard
