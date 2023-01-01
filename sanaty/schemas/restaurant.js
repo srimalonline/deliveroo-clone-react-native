2c1b7d04e93adfe0a5ff2dbd09cce00786611ee7
srimalonline <dilankasrimal23@gmail.com>
2023-01-02 00:16:48 +0530

    refactoring fields

--- sanaty/schemas/restaurant.js
+++ sanaty/schemas/restaurant.js
@@ -38,7 +38,28 @@ export default defineType({
       title: "Restaurant address",
       validation: (Rule) =>Rule.required()
     },
-
+    {
+      name: "rating",
+      type: "number",
+      title: "Enter a Rating from (1-5 Stars)",
+      validation: (Rule) =>
+        Rule.required()
+          .min(1)
+          .max(5)
+          .error("Please enter a Value between 1 and 5")
+    },
+    {
+      name: "type",
+      title: "Category",
+      validation: (Rule) => Rule.required(),
+      type: "reference",
+      to: [{ type: "category"}],
+    },
+    {
+      name: "deshes",
+      type: "array",
+      title: "Dishes",
+      of: [{ type: "reference", to [{ type: "dish "}]]
+    }
   ],
-
 })
