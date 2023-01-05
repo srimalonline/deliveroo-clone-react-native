70ad55ba7b72a49f5fad9cb76ed8de6abdd0a049
srimalonline <dilankasrimal23@gmail.com>
2023-01-06 01:48:55 +0530

    import icons to restaurant screen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -2,7 +2,8 @@ import { ScrollView, View, Text, Image, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { urlFor } from '../sanity'
-import { ArrowLeftIcon } from 'react-native-heroicons/outline'
+import { ArrowLeftIcon, MapPinIcon } from 'react-native-heroicons/outline'
+import { StarIcon  } from "react-native-heroicons/solid";
 
 const RestaurantScreen = () => {
     const navigation = useNavigation();
@@ -39,7 +40,15 @@ const RestaurantScreen = () => {
             className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full">
                 <ArrowLeftIcon size={20} color="#00CCBB"/>
             </TouchableOpacity>
-                <Text>අම්මා</Text>
+      </View>
+      <View className="bg-white">
+        <View className="px-4 pt-4">
+            <Text className="text-3xl font-bold">{title}</Text>
+            <View className="flew-row space-x-2 my-1"></View>
+            <View>
+                <StarIcon color="green" opacity={0.5} size={22} />
+            </View>
+        </View>
       </View>
     </ScrollView>
   )
