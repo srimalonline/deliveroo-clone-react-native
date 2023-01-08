24bc4718369b5ead7d543cc1d467079501e10dbb
srimalonline <dilankasrimal23@gmail.com>
2023-01-08 23:35:56 +0530

    adding image path

--- components/categories.js
+++ components/categories.js
@@ -29,6 +29,7 @@ const Categories = () => {
       {categories.map(category => (
         <CategoryCard
         key={category._id}  
+        imgUrl={urlFor(category.image).width(200).url()}
         title={category.title} />
       ))}
 
