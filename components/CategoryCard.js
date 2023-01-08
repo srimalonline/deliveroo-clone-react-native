72acb8aac5ebf3c9afbb13499f8248095de16991
srimalonline <dilankasrimal23@gmail.com>
2023-01-09 02:21:13 +0530

    enabling cetegory image sucessfully

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -7,9 +7,9 @@ const CategoryCard = ({ imgUrl, title }) => {
   return (
     <TouchableOpacity className="relative mr-2 bg-orange-600">
         <Image
-            // source={{
-            //     uri: urlFor(imgUrl).url(),
-            // }}
+            source={{
+                uri: urlFor(imgUrl).width(200).url(),
+            }}
             className="h-20 w-20 rounded"/>
         <Text className="absolute bottom-1 left-1 text-white font-bold">
             {title}
