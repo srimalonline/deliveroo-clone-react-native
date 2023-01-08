5c0a400194f63ce8997ba92f2f3ec97f0de6db92
srimalonline <dilankasrimal23@gmail.com>
2023-01-09 01:11:29 +0530

    Dishrow content adding

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,12 +1,17 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import Curreny from 'react-currency-formatter'
 
 const DishRow = ({
     id, name, description, price, image
 }) => {
   return (
     <TouchableOpacity>
-      <Text>DishRow</Text>
+      <Text className="text-lg mb-1">{name}</Text>
+      <Text className="text-gray-400">{description}</Text>
+      <Text>
+        <Curreny quantity={price} currency="LKR"/>
+      </Text>
     </TouchableOpacity>
   )
 }
