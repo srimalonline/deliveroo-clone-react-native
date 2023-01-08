795935cde2ee3849c99f7b46c05f5168b0351af5
srimalonline <dilankasrimal23@gmail.com>
2023-01-09 00:43:12 +0530

    dishrow props

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -74,7 +74,14 @@ const RestaurantScreen = () => {
 
         {/* Dishrows */}
         {dishes.map(dish => (
-          
+          <DishRow 
+            key={dish._id}
+            id={dish._id}
+            name={dish.name}
+            description={dish.short_description}
+            price={dish.price}
+            image={dish.image}
+          />
         ))}
       </View>
     </ScrollView>
