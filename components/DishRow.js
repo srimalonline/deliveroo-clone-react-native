95c6ef8327beb595da86c00d4dd6ce9973d5fed8
srimalonline <dilankasrimal23@gmail.com>
2023-01-13 10:43:17 +0530

    plus minus styleing

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,15 +1,20 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Curreny from "react-currency-formatter";
 import { urlFor } from "../sanity";
-import { MinusCircleIcon, PlusCircleIcon } from "react-native-heroicons/solid"
+import { MinusCircleIcon, PlusCircleIcon } from "react-native-heroicons/solid";
 
 const DishRow = ({ id, name, description, price, image }) => {
   const [isPressed, setIsPressed] = useState(false);
 
   return (
     <>
-      <TouchableOpacity className="bg-white border p-4 border-gray-200" onPress={() =>        setIsPressed(!isPressed)}>
+      <TouchableOpacity
+        className={`bg-white border p-4 border-gray-200 ${
+          isPressed && "border-b-0"
+        }`}
+        onPress={() => setIsPressed(!isPressed)}
+      >
         <View className="flex-row ">
           <View className="flex-1 or-2">
             <Text className="text-lg mb-1">{name}</Text>
@@ -34,15 +39,23 @@ const DishRow = ({ id, name, description, price, image }) => {
         </View>
       </TouchableOpacity>
       {isPressed && (
-        <View>
-          <View>
+        <View className="bg-white px-4">
+          <View className="flex-row items-center space-x-2 pb-3">
             <TouchableOpacity>
-             <Text> Text
-             <MinusCircleIcon 
-            //  color={items.length > 0 ? "#00CCBB" : "gray"} 
-            color="#00CCBB"
-             size={40}/>
-             </Text>
+              <MinusCircleIcon 
+                //  color={items.length > 0 ? "#00CCBB" : "gray"}
+                color="#00CCBB"
+                size={40}
+              />
+            </TouchableOpacity>
+
+            <Text>0</Text>
+            <TouchableOpacity>
+              <PlusCircleIcon
+                //  color={items.length > 0 ? "#00CCBB" : "gray"}
+                color="#00CCBB"
+                size={40}
+              />
             </TouchableOpacity>
           </View>
         </View>
