9b555dfd7f1497159689a068d359828616448518
srimalonline <dilankasrimal23@gmail.com>
2023-01-13 11:53:28 +0530

    declaring items variable with their functions

--- features/basketSlice.js
+++ features/basketSlice.js
@@ -1,25 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  value: 0,
+  items: [],
 }
 
 export const basketSlice = createSlice({
   name: 'basket',
   initialState,
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1
+    addToBasket: (state, action) => {
+      state.items += 1 [...state.items, action.payload]; 
     },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload
+    removeFromBasket: (state, action) => {
+      state.items -= 1
     },
   },
 })
