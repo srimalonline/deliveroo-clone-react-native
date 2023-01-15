34453c97356ed52188d3d88eba2d5fb3e93aa3a3
srimalonline <dilankasrimal23@gmail.com>
2023-01-16 01:08:51 +0530

    adding currencies to BasketIcon

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -1,8 +1,9 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { selectBasketItems, selectBasketTotal } from '../features/basketSlice'
 import { useNavigation } from '@react-navigation/native'
+import Curreny from "react-currency-formatter";
 
 const BasketIcon = () => {
     const items = useSelector(selectBasketItems);
@@ -11,7 +12,13 @@ const BasketIcon = () => {
 
   return (
     <View className="absolute bottom-10 w-full z-50">
-      <Text>BasketIcon</Text>
+      <TouchableOpacity className="bg-[#00CCBB] mx-5 flex-row items-center">
+      <Text>{items.length}</Text>
+      <Text>View Basket</Text>
+      <Text className="text-lg text-white font-extrabold">
+        <Curreny quantity={basketTotal} currency="LKR"/>
+      </Text>
+      </TouchableOpacity>
     </View>
   )
 }
