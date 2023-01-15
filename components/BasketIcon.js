c053e084081427182cbb9f09898644e74714ec88
srimalonline <dilankasrimal23@gmail.com>
2023-01-16 00:39:24 +0530

    basket total useSelector

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -1,12 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { selectBasketItems } from '../features/basketSlice'
+import { selectBasketItems, selectBasketTotal } from '../features/basketSlice'
 import { useNavigation } from '@react-navigation/native/lib/typescript/src'
 
 const BasketIcon = () => {
     const items = useSelector(selectBasketItems);
     const navigation = useNavigation();
+    const basketTotal = useSelector(selectBasketTotal);
   return (
     <View>
       <Text>BasketIcon</Text>
