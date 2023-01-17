516356060d707a7dc48bb0056f58107ada0c394f
srimalonline <dilankasrimal23@gmail.com>
2023-01-17 18:52:31 +0530

    create basket screen

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
+import BasketScreen from "./screens/BasketScreen";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
@@ -19,6 +20,7 @@ export default function App() {
           <Stack.Navigator>
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+            <Stack.Screen name="Basket" component={BasketScreen} />
           </Stack.Navigator>
         </TailwindProvider>
       </Provider>
