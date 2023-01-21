03b59777bf28e905010b1ff3ddbbd414505e0ad2
srimalonline <dilankasrimal23@gmail.com>
2023-01-21 23:09:12 +0530

    basket view errors

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,12 +1,11 @@
-import { ScrollView, View, Text } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
 
 const BasketScreen = () => {
   return (
-    <ScrollView horizontal
-    className="flex-row">
+    <View>
       <Text>BasketScreen</Text>
-    </ScrollView>
+    </View>
   )
 }
 
