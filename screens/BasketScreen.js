e4015481fbea24d09101e506022f88540a50b147
srimalonline <dilankasrimal23@gmail.com>
2023-01-22 11:03:02 +0530

    adding navigation

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,11 +1,13 @@
 import { ScrollView, View, Text } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const BasketScreen = () => {
+  const navigation = useNavigation();
   return (
-    <ScrollView>
+    <View>
       <Text>BasketScreen</Text>
-    </ScrollView>
+    </View>
   )
 }
 
