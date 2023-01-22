aad580081ee0504510edb8ed0834b559c08be282
srimalonline <dilankasrimal23@gmail.com>
2023-01-22 11:09:24 +0530

    restaurantSlice

---
+++ features/restaurantSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  restaurant: {
+    id: null,
+    imgUrl: null,
+    title: null,
+    rating: null,
+    genre: null,
+    address: null,
+    short_description: null,
+    dishes: null,
+  },
+}
+
+export const restaurantSlice = createSlice({
+  name: 'restaurant',
+  initialState,
+  reducers: {
+    setRestaurant: (state, action) => {
+        state.restaurant = action.payload;
+    }
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { addToBasket, removeFromBasket } = restaurantSlice.actions;
+
+export const selectBasketItems = (state) => state.basket.items;
+
+export const selectBasketItemsWithId = (state, id) => 
+  state.basket.items.filter((item) => item.id === id);
+
+export const selectBasketTotal = (state) =>state.basket.items.reduce((total, item) => 
+total += item.price, 0)
+
+export default restaurantSlice.reducer;
