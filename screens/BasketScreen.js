442eb7b535fec0a022ceb392a155a0c6ea2bd4e5
srimalonline <dilankasrimal23@gmail.com>
2023-01-22 11:44:43 +0530

    changing memo function to useEffect

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -4,20 +4,25 @@ import { useNavigation } from '@react-navigation/native'
 import { useDispatch, useSelector } from 'react-redux';
 import { selectRestaurant } from '../features/restaurantSlice';
 import { selectBasketItems } from '../features/basketSlice';
-import { useMemo } from 'react';
+import { useEffect } from 'react';
 
 const BasketScreen = () => {
   const navigation = useNavigation();
   const restaurant = useSelector(selectRestaurant);
   const items = useSelector(selectBasketItems);
+  cont [groupedItemsInBasket, setGroupedItemsInBasket] = useState([]);
   const dispath = useDispatch();
 
-  useMemo(() => {
+  useEffect(() => {
     const groupedItems = items.reduce((results, item) => {
       (results[item.id] = results[item.id] || []).push(item);
       return results;
-    }, {} )
+    }, {} );
+
+    setGroupedItemsInBasket(groupedItems);
   },[items])
+
+  console.log(groupedItemsInBasket);
 
   return (
     <View>
