4cb4ae6702d23615c0dc739bb2836fa3d8307a5f
srimalonline <dilankasrimal23@gmail.com>
2023-03-16 21:38:12 +0530

    animatable image and text

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,10 +1,21 @@
 import { SafeAreaView ,View, Text } from 'react-native'
 import React from 'react'
+import * as Animatable from "react-native-animatable"
 
 const PreparingOrderScreen = () => {
   return (
     <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center">
-      <Text>PreparingOrderScreen</Text>
+        <Animatable.Image 
+            source={require("../assets/deliveroodribbbble.gif")}
+            animation="slideInUp"
+            iterationCount={1}
+            className="h-96 w-96"
+        />
+      <Animatable.Text
+        animation="slideInUp"
+        iterationCount={1}
+        className="text-lg my-10 text-white font-bold text-center"
+      >Waiting for Restaurant to accept your order!</Animatable.Text>
     </SafeAreaView>
   )
 }
