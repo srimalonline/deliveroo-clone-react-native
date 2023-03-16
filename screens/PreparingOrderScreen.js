c21bd1405b42b27e77edbf4e59dc5809bf0420e8
srimalonline <dilankasrimal23@gmail.com>
2023-03-16 21:24:21 +0530

    styling the componenet

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,11 +1,11 @@
-import { View, Text } from 'react-native'
+import { SafeAreaView ,View, Text } from 'react-native'
 import React from 'react'
 
 const PreparingOrderScreen = () => {
   return (
-    <View>
+    <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center">
       <Text>PreparingOrderScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
