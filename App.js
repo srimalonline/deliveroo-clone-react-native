643c1bc5472dc5a807626f579dbde3173d4a41ff
srimalonline <dilankasrimal23@gmail.com>
2023-03-16 21:12:47 +0530

    adding PreparingOrderScreen

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
 import BasketScreen from "./screens/BasketScreen";
+import PreparingOrderScreen from "./screens/PreparingOrderScreen";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
@@ -21,7 +22,10 @@ export default function App() {
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Restaurant" component={RestaurantScreen} />
             <Stack.Screen name="Basket" component={BasketScreen} 
-            options={{ presentation: 'modal', headerShown: false }}/>
+            options={{ presentation: 'model', headerShown: false }}
+            />
+            <Stack.Screen name="PreparingOrderScreen" component={PreparingOrderScreen}
+            options={{ presentation: "fullScreenModal", headerShown: false }}/>
           </Stack.Navigator>
         </TailwindProvider>
       </Provider>
