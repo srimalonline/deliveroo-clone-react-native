e8f9079bf19ebacb0b3ebd4eca229b9f097965bb
srimalonline <dilankasrimal23@gmail.com>
2023-03-16 22:37:40 +0530

    adding estimted arrival box

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { SafeAreaView, View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
+import { selectRestaurant } from "../features/restaurantSlice";
+import { XCircleIcon } from "react-native-heroicons/solid";
 
 const DeliveryScreen = () => {
+  const navigation = useNavigation();
+  const restaurant = useSelector(selectRestaurant);
   return (
-    <View>
-      <Text>DeliveryScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-[#00CCBB] flex-1">
+      <View className="z-50 p-5 pt-10">
+        <View className="flex-row justify-between items-center">
+          <TouchableOpacity>
+            <XCircleIcon color="white" height={50} width={50} />
+          </TouchableOpacity>
+          <Text className="text-white text-lg">Order Help</Text>
+        </View>
 
-export default DeliveryScreen
+        <View className="bg-white mx-5 my-2 rounded-md p-6 z-50 shadow-md">
+          <View>
+            <Text className="text-lg text-gray-400">Estimated Arrival</Text>
+            <Text className="text-4xl font-bold">30-40 min</Text>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default DeliveryScreen;
