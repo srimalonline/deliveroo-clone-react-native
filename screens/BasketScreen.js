0a6c5c9554504486b604ffa42e7d56abbe0b42f9
srimalonline <dilankasrimal23@gmail.com>
2023-03-16 19:56:14 +0530

    deliver time change view

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,11 +1,17 @@
-import {  SafeAreaView,View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
-import { useDispatch, useSelector } from 'react-redux';
-import { selectRestaurant } from '../features/restaurantSlice';
-import { selectBasketItems } from '../features/basketSlice';
-import { useEffect, useState } from 'react';
-import { XCircleIcon } from 'react-native-heroicons/solid'
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
+import { selectRestaurant } from "../features/restaurantSlice";
+import { selectBasketItems } from "../features/basketSlice";
+import { useEffect, useState } from "react";
+import { XCircleIcon } from "react-native-heroicons/solid";
 
 const BasketScreen = () => {
   const navigation = useNavigation();
@@ -18,29 +24,44 @@ const BasketScreen = () => {
     const groupedItems = items.reduce((results, item) => {
       (results[item.id] = results[item.id] || []).push(item);
       return results;
-    }, {} );
+    }, {});
 
     setGroupedItemsInBasket(groupedItems);
-  },[items])
+  }, [items]);
 
   return (
     <SafeAreaView className="flex-1 bg-white">
-     <View  className="flex-1 bg-gray-100">
-      <View className="p-5 border-[#00CCBB] bg-white shadow-sm">
-        <View>
-          <Text className="text-lg font-bold text-center">Basket</Text>
-          <Text className="text-center text-gray-400">{restaurant.title}</Text>
+      <View className="flex-1 bg-gray-100">
+        <View className="p-5 border-[#00CCBB] bg-white shadow-sm">
+          <View>
+            <Text className="text-lg font-bold text-center">Basket</Text>
+            <Text className="text-center text-gray-400">
+              {restaurant.title}
+            </Text>
+          </View>
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className="rounded-full bg-gray-100 absolute top-3 right-5"
+          >
+            <XCircleIcon color="#00CCBB" height={50} width={50} />
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity
-        onPress={navigation.goBack}
-        className="rounded-full bg-gray-100 absolute top-3 right-5"
-        >
-          <XCircleIcon color="#00CCBB" height={50} width={50}/>
-        </TouchableOpacity>
-      </View>
-     </View>
-    </SafeAreaView>
-  )
-}
 
-export default BasketScreen
+        <View className="flex-row items-center space-x-4 px-4 py-4 bg-white my-5">
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-4 bg-gray-300 p-4 rounded-full"
+          />
+          <Text className="flex-1">Deliver in 50-75 min</Text>
+          <TouchableOpacity>
+            <Text className="text-[#00CCBB]">Change</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default BasketScreen;
